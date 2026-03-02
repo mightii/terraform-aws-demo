3e85b52379ce6c7a4c8b4195de0ecbc5e70f8dbf
mightii <69608364+mightii@users.noreply.github.com>
2026-03-02 18:41:51 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "demo"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance (Amazon Linux 2 on eu-west-1)"
+  type        = string
+  default     = "ami-0905a3c97561e0b69"
+}
