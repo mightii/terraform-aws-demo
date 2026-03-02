4846b7d699501f9c6ed5762f4ee252e42064c453
mightii <69608364+mightii@users.noreply.github.com>
2026-03-02 18:42:20 +0100

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.demo.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.demo.public_ip
+}
+
+output "ec2_instance_id" {
+  description = "Instance ID of the EC2"
+  value       = aws_instance.demo.id
+}
